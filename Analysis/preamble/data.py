62b8fa2982ee723b81f37f463cf417d4eca10d7f
Joel Ong <joelong@hawaii.edu>
2023-04-19 14:08:58 -1000

    gradient kernel figures

--- Analysis/preamble/data.py
+++ Analysis/preamble/data.py
@@ -1,9 +1,13 @@
 import numpy as np
 
-data = np.load("data/TIC350842552.npy", allow_pickle=True)[()]
-ν = data['f']
-PS = data['p'] * 1e6
-del(data)
+try:
+	data = np.load("data/TIC350842552.npy", allow_pickle=True)[()]
+	ν = data['f']
+	PS = data['p'] * 1e6
+	del(data)
+except FileNotFoundError:
+	ν = None
+	PS = None
 
 # ν, PS = np.loadtxt("diamonds_peakbagging/DIAMONDS/data/TIC350842552.txt").T
 # PS *= 1e6
